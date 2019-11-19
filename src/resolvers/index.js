29c81e5989f6e2790893e653387ec39da3f3fdc3
Hesam Bayat <hesam.bayat@gmail.com>
2019-11-19 19:41:44 +0400

    Bind Course to fragments

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,11 @@
 import { extractFragmentReplacements } from 'prisma-binding'
+import Course from './Course'
 import Mutation from './Mutation'
 import Query from './Query'
 import User from './User'
 
 const resolvers = {
+  Course,
   Mutation,
   Query,
   User
